1f99c4f6c107810a4f829311ae06942ee9ebcf69
Palash Kumer <palashkumer237@gmail.com>
2023-12-01 12:00:04 +0600

    fix: latest update

--- custom-currency-converter-.php
+++ custom-currency-converter-.php
@@ -11,7 +11,9 @@ Author URI: https://github.com/palashkumer
 if (!defined('ABSPATH')) {
     exit;
 }
-// Enqueue styles and scripts
+/**
+ * Enqueue styles and scripts
+ */
 function currency_converter_enqueue_scripts() {
     wp_enqueue_style('currency-converter-style', plugins_url('assets/css/style.css', __FILE__));
     wp_enqueue_script('currency-converter-script', plugins_url('assets/js/script.js', __FILE__), array('jquery'), null, true);
@@ -21,16 +23,22 @@ function currency_converter_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'currency_converter_enqueue_scripts');
 
-// Add AJAX action for currency conversion
+/**
+ * Add AJAX action for currency conversion
+ */
 add_action('wp_ajax_convert_currency', 'convert_currency');
 add_action('wp_ajax_nopriv_convert_currency', 'convert_currency');
 
 
-// Include AJAX functions
+/**
+ * Include AJAX functions
+ */
 include plugin_dir_path(__FILE__) . 'includes/ajax-function.php';
 
 
-// Add shortcode for displaying the currency converter
+/**
+ * Add shortcode for displaying the currency converter
+*/
 function currency_converter_shortcode() {
     ob_start();
     include 'templates/currency-converter-template.php'; 
